7d9107da81741a9fe46efe1837a000048a1f9c30
Ruben Arutyunyan <ruben.arutyunyan@gmail.com>
2021-04-08 22:12:54 +0300

    решения задач 3 модуля

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,17 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let result = [];
+  if (a >= b) {
+    for (let number of arr) {
+      if (number >= b && number <= a) {
+        result.push(number);
+      }
+    }
+  } else {
+    for (let number of arr) {
+      if (number >= a && number <= b) {
+        result.push(number);
+      }
+    }
+  }
+  return result;
 }
